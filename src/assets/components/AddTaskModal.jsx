5b66192f5eae8e77f77f7abbe7d78812e618a359
Dingos <gabriribi676@gmail.com>
2026-08-05 18:38:45 -0300

    feat: enhance AddTaskModal with validation and error handling for task name and due date

--- src/assets/components/AddTaskModal.jsx
+++ src/assets/components/AddTaskModal.jsx
@@ -1,49 +1,99 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import dayjs from 'dayjs';
 
 function AddTaskModal({ setIsModalOpen, setTasks }) {
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [errors, setErrors] = useState({});
   const { t } = useTranslation();
 
-  function handleKeyDown(event) {
-    if (event.key == 'Escape') {
-      setIsModalOpen(false);
+  function validateName(name) {
+    if (name.trim() === "") return "Task name is empty"
+    return null
+  }
+
+  function validateDate(date) {
+    if (!date) {
+      return "Enter valid date"
     }
-  };
+
+    if (dayjs(date).isBefore(dayjs())) {
+      return dayjs().format("Enter a date starting from YYYY/mm/dd")
+    }
+
+    return null
+  }
+
+  function validateForm({ name, dueDate }) {
+    return {
+      name: validateName(name),
+      date: validateDate(dueDate)
+    };
+  }
+
+  function handleNameChange(event) {
+    if (!hasSubmitted) return;
+
+    setErrors(prev => ({
+      ...prev,
+      name: validateName(event.target.value),
+    }));
+  }
+
+  function handleDateChange(event) {
+    if (!hasSubmitted) return;
+
+    setErrors(prev => ({
+      ...prev,
+      dueDate: validateName(event.target.value),
+    }));
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
 
+    setHasSubmitted(true);
+
     const taskInfo = event.target;
+    const task = {
+      id: dayjs(),
+      name: taskInfo.taskName.value,
+      description: taskInfo.description.value,
+      dueDate: taskInfo.dueDate.value ? dayjs(taskInfo.dueDate.value).format('YYYY-MM-DD') : null,
+      priority: taskInfo.priority.value,
+      completed: false,
+    }
+    const err = validateForm(task);
+    const isValid = Object.values(err).every(error => error === null);
+
+    setErrors(err);
+
+    if (!isValid) return;
+
     setTasks(prevTasks => [
       ...prevTasks,
-      {
-        id: dayjs(),
-        name: taskInfo.taskName.value,
-        description: taskInfo.description.value,
-        dueDate: taskInfo.dueDate.value ? dayjs(taskInfo.dueDate.value).format('YYYY-MM-DD') : null,
-        priority: taskInfo.priority.value,
-        completed: false,
-      }
+      task
     ]);
-    
+
     setIsModalOpen(false);
   }
 
-  const dateInputRef = useRef(null);
-
+  const today = dayjs().format("YYYY-MM-DD");
+  const hasNameError = hasSubmitted && errors.name
+  const hasDateError = hasSubmitted && errors.date
   useEffect(() => {
-    const dateElement = dateInputRef.current;
-    const today = dayjs().format('YYYY-MM-DD');
-    dateElement.min = today;
-    dateElement.value = today;
-    
+    function handleKeyDown(event) {
+      if (event.key == 'Escape') {
+        setIsModalOpen(false);
+      }
+    };
+
     window.addEventListener('keydown', handleKeyDown);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  });
+  }, [setIsModalOpen]);
 
   return (
     <div className="fixed bg-black/50 flex items-center justify-center inset-0">
@@ -51,16 +101,55 @@ function AddTaskModal({ setIsModalOpen, setTasks }) {
         <h2 className="text-xl font-bold mb-4">{t('addTask.label')}</h2>
 
         <form onSubmit={handleSubmit}>
-          <label htmlFor="taskName" className="block text-gray-600">{t('addTask.taskName')}</label>
-          <input type="text" name="taskName" id="taskName" className="border border-gray-300 rounded-md py-2 px-3 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-          
-          <label htmlFor="description" className="block text-gray-600 mt-4">{t('addTask.taskDescription')}</label>
-          <input type="text" name="description" id="description" className="border border-gray-300 rounded-md py-2 px-3 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder={t('addTask.optional')} />
+          <label
+            htmlFor="taskName"
+            className={`block ${hasNameError
+              ? "text-red-600"
+              : "text-gray-600"
+              } `}>{t('addTask.taskName')} <span className='text-xs'>{t('addTask.required')}</span></label>
+          <input
+            type="text"
+            name="taskName"
+            id="taskName"
+            className={`border ${hasNameError
+              ? "border-red-600"
+              : "border-gray-300"
+              } rounded-md py-2 px-3 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            onChange={handleNameChange}
+          />
+          {hasNameError && (
+            <p
+              className="text-xs mt-1 text-red-600"
+            >{errors.name}</p>
+          )}
+
+          <label htmlFor="description" className="block text-gray-600 mt-4">{t('addTask.taskDescription')} <span className='text-xs'>{t('addTask.optional')}</span></label>
+          <input type="text" name="description" id="description" className="border border-gray-300 rounded-md py-2 px-3 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
 
           <div className="flex items-center mt-4 gap-x-4">
             <div>
-              <label htmlFor="dueDate" className="block text-gray-600 mt-4">{t('addTask.dueDate')}</label>
-              <input ref={dateInputRef} type="date" name="dueDate" id="dueDate" className="border border-gray-300 rounded-md py-2 px-3 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+              <label
+                htmlFor="dueDate"
+                className={`block ${hasDateError
+                  ? "text-red-600"
+                  : "text-gray-600"}
+                mt-4`}>{t('addTask.dueDate')}</label>
+              <input
+                type="date"
+                onChange={handleDateChange}
+                min={today}
+                defaultValue={today}
+                name="dueDate"
+                id="dueDate"
+                className={`border ${hasDateError
+                  ? "border-red-600 text-red-600"
+                  : "border-gray-300 text-black"
+                  } rounded-md py-2 px-3 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500`} />
+              {hasDateError && (
+                <p
+                  className="text-xs mt-1 text-red-600"
+                >{errors.date}</p>
+              )}
             </div>
             <div>
               <label htmlFor="priority" className="block text-gray-600 mt-4">{t('addTask.priority')}</label>
